a343aef7e3da216fde6cf90afaa1b660ebdf0549
Fedorov Anton <afedorov43@gmail.com>
2022-11-27 19:46:46 +0300

    added dictionaries

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is a sample Python script.
+import json
+
 import requests
 from authentication import auth_headers
+from dictionaries import engines
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -17,13 +20,20 @@ def print_hi(name):
     r5 = requests.get(url="http://iss.moex.com/iss/securities.json", headers=auth_headers)
     # print(r.content.decode())
     # print(r2.content.decode())
-    r3 = requests.get(url="http://iss.moex.com/iss/engines/stock/markets/bonds/trades/boards.json",
+    r3 = requests.get(url="https://iss.moex.com/iss/history/engines/stock/markets/bonds/boards.json",
                       headers=auth_headers)
-    result = r3.content.decode()
+    result = (r.content.decode())
     # print(type(result))
-    print(result)
+    # print(result)
     # print(r4.content.decode())
     # print(r5.content.decode())
+
+    for engine in engines:
+        request = requests.get(
+            url=f"http://iss.moex.com/iss/engines/{engine}/markets.json"
+        )
+        print(engine)
+        print(request.content.decode())
 
 
 # Press the green button in the gutter to run the script.
